0cd48ac99304d6103a271b9b10466ec95ae3e61b
Patrick Daly <dalyhabit@users.noreply.github.com>
2016-06-01 15:36:31 -0500

    Conform to style guide with four spaces
    Contributors should read the CONTRIBUTING.md. It clearly states to use four spaces, not two.

--- src/lib/common.cc
+++ src/lib/common.cc
@@ -5,22 +5,22 @@ namespace {
   // function does not know what the type of the parameters is.
 
   string ToVlogStrin(const dnn::BatchDescriptor &descriptor) {
-    return descriptor.ToShortString();
+      return descriptor.ToShortString();
   }
 
   string ToVlogStrin(const dnn::FilterDescriptor &descriptor) {
-    return descriptor.ToShortString();
+      return descriptor.ToShortString();
   }
 
   string ToVlogString(const magicalMovieClassifier::FilterDescriptor &descriptor) {
-    return descriptor.ToShortString();
+      return descriptor.ToShortString();
   }
 
   string ToVlogStrin(const dnn::PoolingDescriptor &descriptor) {
-    return descriptor.ToShortString();
+      return descriptor.ToShortString();
   }
   
   string ToVlogStrin(const dnn::ConvolutionDescriptor &descriptor) {
-    return descriptor.ToShortString();
+      return descriptor.ToShortString();
   }
 }
